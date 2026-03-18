225f3674caf736bad6380d8eb1baf58b65f358dd
khnbutt118-dev <khnbutt118@gmail.com>
2026-03-18 23:32:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-import app as st
+import streamlit as st
 
 genai.configure(api_key="AIzaSyDLS9crwogs47DR6AkIOeSN5C3gqINk97w")
 model = genai.GenerativeModel("gemini-2.5-flash")
